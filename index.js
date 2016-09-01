1073ad81472a00b3f2f3a2999eb4650731f629ce
Kha Nguyen <ykha@uw.edu>
2016-09-01 00:53:07 -0700

    update style

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
+app.get('/about', function(req, res) {
+   res.render('about');
+});
+
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate');

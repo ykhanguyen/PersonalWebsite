9ed648edf9cef501d801d7d710331886de033e8b
Kha Nguyen <ykha@uw.edu>
2016-08-31 23:51:01 -0700

    fix vertical

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.engine('handlebars', handlebars.engine);
 
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3333);
 
 app.use(express.static(__dirname + '/public'));
 
